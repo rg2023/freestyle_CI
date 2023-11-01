2e84dfdb0caacb1950900bb2a7aa8de2a155fc93
rachel <rachel058326@gmail.com>
2023-11-01 10:35:59 +0200

    update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('./database');
 
 // Server is listening
 //----------
+//==========
 app.listen(app.get('port'), () => {
   console.log('Server on port', app.get('port'));
   console.log('Environment:', process.env.NODE_ENV);
