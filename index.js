e94221b0fa9a121296dbc24640b4d4a455132292
rachel <rachel058326@gmail.com>
2023-11-01 13:31:36 +0200

    change

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const app = require('./server');
 require('./database');
 
+
 // Server is listening
 app.listen(app.get('port'), () => {
   console.log('Server on port', app.get('port'));
